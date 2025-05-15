bd45f5549b8a9de72262e52bad616f2c9364ba04
alessandrocab <alessandrocabriolu@hotmail.com>
2025-05-15 14:43:27 +0200

    Update SecretReveal.js

--- src/SecretReveal.js
+++ src/SecretReveal.js
@@ -40,7 +40,7 @@ export default function SecretReveal() {
           }}
         >
           <h1 style={{ fontSize: "1.25rem", fontWeight: "bold", marginBottom: "1rem" }}>
-            Per svelare il segreto del luogo nascosto inserisci la combinazione corretta
+            Per svelare il segreto del luogo nascosto inserisci la combinazione corrett
           </h1>
           <input
             type="text"
